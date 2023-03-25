ec0a0b62e9b613bf00b798e08229dd3be019b642
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2023-03-25 19:33:13 -0300

    modificado app

--- smart-store-react/src/App.jsx
+++ smart-store-react/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import Header from './components/header'
 
 function App() {
 
   return (
-    <div className="App">
-        <h1>aaaaa</h1>
+    <div>
+        <Header />
     </div>
   )
 }
