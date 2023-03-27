eec034cf976c3c7b9cafee7d0fa73253416b80e4
Lucas Cassimiro <109998550+lucas-cassimiro@users.noreply.github.com>
2023-03-27 18:30:13 -0300

    atualizado card de novidades (#13)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Banner from '../../components/banner'
-import News from '../../components/novidades/novidades'
+import News from '../../components/news'
 
 const Home = () => {
   return (
