70fce6dd005711183448d3f2c1a9fdf7a0f0dc1d
Lucas Cassimiro <109998550+lucas-cassimiro@users.noreply.github.com>
2023-03-27 14:44:33 -0300

    removido erro e index.css (#7)
    Co-authored-by: Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>

--- smart-store-react/src/components/header/styles.js
+++ smart-store-react/src/components/header/styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link, NavLink } from "react-router-dom";
-import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
