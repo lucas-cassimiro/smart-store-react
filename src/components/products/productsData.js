72211ff6fa165d394ce5f1bc1b6b5f9d62d4ef22
Lucas Cassimiro <109998550+lucas-cassimiro@users.noreply.github.com>
2023-03-28 20:58:48 -0300

    Organizando imagens (#18)
    * arrumado
    
    * criando banner para black friday
    
    * organizando imagens, renomeando e refatorando
    
    * organizando imagens e criando carrousel
    
    ---------
    
    Co-authored-by: Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>

---
+++ src/components/products/productsData.js
@@ -0,0 +1,59 @@
+import CaixaJbl from "../../assets/img/banner-products-som-jbl.png";
+import Iphone from "../../assets/img/banner-products-iphone-image.png";
+import SamsungS22 from "../../assets/img/banner-products-samsung-galaxy.png";
+import AppleWatch from "../../assets/img/banner-products-apple-watch-series.png";
+import OculosVR from "../../assets/img/banner-products-oculos-vr.png";
+import CarregadorIphone from "../../assets/img/banner-products-carregador-iphone.png";
+import SamsungM53 from "../../assets/img/banner-products-smartphone-samsung.png";
+import FoneXiaomi from "../../assets/img/banner-products-fone-de-ouvido.png";
+
+export const products = [
+  {
+    name: "Caixa de som JBL Boombox 2 Preta",
+    price: 2999.99,
+    discount: 25,
+    img: CaixaJbl,
+  },
+  {
+    name: "iPhone 13 Mini Azul",
+    price: 4999.99,
+    discount: 20,
+    img: Iphone,
+  },
+  {
+    name: "Samsung Galaxy S22 Ultra 5G",
+    price: 6999.99,
+    discount: 20,
+    img: SamsungS22,
+  },
+  {
+    name: "Apple Watch Series 6 44mm Azul",
+    price: 2999.99,
+    discount: 33,
+    img: AppleWatch,
+  },
+  {
+    name: "VR Box, Óculos VR para Smartphone",
+    price: 129.99,
+    discount: 23,
+    img: OculosVR,
+  },
+  {
+    name: "Carregador iPhone Tipo C - Turbo",
+    price: 399.99,
+    discount: 12,
+    img: CarregadorIphone,
+  },
+  {
+    name: "Smartphone Samsung Galaxy M53 5G",
+    price: 2850.14,
+    discount: 48,
+    img: SamsungM53,
+  },
+  {
+    name: "Fone de Ouvido Xiaomi Earbuds Basic 2",
+    price: 199.99,
+    discount: 25,
+    img: FoneXiaomi,
+  }
+];
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import * as S from "./styles";
 import Banner from "../../components/banner";
 import News from "../../components/news";
 import BlackFridayBanner from "../../assets/img/banner-airpods-black-friday.png";
+import ProductsBanner from "../../components/products";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <News />
       <S.BlackFriday>Black Friday</S.BlackFriday>
       <S.BlackFridayBannerImage src={BlackFridayBanner} alt="Banner Black Friday" />
+      <ProductsBanner />
     </S.HomeContainer>
   );
 };
