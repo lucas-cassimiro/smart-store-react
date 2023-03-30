3ca7d35679fe2d398bde464c8ca499e3206ef8af
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2023-03-30 19:52:30 -0300

    estilização do products

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,40 +1,49 @@
+import * as S from "./styles";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, A11y, Autoplay } from "swiper";
+import { Navigation, A11y, Autoplay } from "swiper";
 import { products } from "./productsData";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
 
 const ProductsBanner = () => {
   return (
     <div>
-      <Swiper
+      <S.SwiperContainer
         modules={[Navigation, A11y, Autoplay]}
-        spaceBetween={30}
-        slidesPerView={3}
+        spaceBetween={24}
+        slidesPerView={4}
         navigation
         // autoplay={{ delay: 4000, disableOnInteraction: false }}
       >
         {products.map((product) => (
           <SwiperSlide key={product.name}>
-            <div>
+            <S.CardDiv>
               <img src={product.img} alt={product.name} />
-              <div>
-                <p>-{product.discount}%</p>
-                <p>Black Friday</p>
-              </div>
-              <p>{product.name}</p>
-              starssssssss
-              <p>R$ {product.price}</p>
-              <p>R$ {((product.price*(100 - product.discount))/100).toFixed(2)}</p>
-              <p>Ou 12x de R$ {(((product.price*(100 - product.discount))/100)/11).toFixed(2)}</p>
-            </div>
+              <S.DescriptionContainer>
+                <div>
+                  <p>-{product.discount}%</p>
+                  <p className="black-friday">Black Friday</p>
+                </div>
+                <h4>{product.name}</h4>
+                starssssssss
+                <p className="real-price">R$ {product.price}</p>
+                <p className="discount-price">
+                  R${" "}
+                  {((product.price * (100 - product.discount)) / 100).toFixed(
+                    2
+                  )}
+                </p>
+                <p className="parcelas">
+                  Ou 12x de R${" "}
+                  {(
+                    ((product.price * (100 - product.discount)) / 100 / 12) *
+                    1.02
+                  ).toFixed(2)}
+                </p>
+              </S.DescriptionContainer>
+            </S.CardDiv>
           </SwiperSlide>
         ))}
-      </Swiper>
+      </S.SwiperContainer>
     </div>
   );
 };
