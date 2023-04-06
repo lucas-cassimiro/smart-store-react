fe8b87df5ccdb6633362f29d31f1010ca01fb369
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-04-06 15:52:54 -0300

    arrumado

--- src/components/Swipers/SwiperBlackFridayProducts/index.jsx
+++ src/components/Swipers/SwiperBlackFridayProducts/index.jsx
@@ -4,7 +4,7 @@ import { SwiperSlide } from "swiper/react";
 import { Navigation, A11y, Autoplay } from "swiper";
 import { products } from "./blackFridayProductsData";
 import SwiperSwitchButton from "../SwiperSwitchButton";
-import CardProducts from "../../CardProducts";
+import CardProducts from "../../cardProducts";
 
 const SwiperBlackFridayProducts = () => {
   return (
